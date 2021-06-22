97c04e85728565e91befcd7ed9fd5e0dbfdcd51d
Ravikiran120 <85937120+Ravikiran120@users.noreply.github.com>
2021-06-22 20:31:30 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,16 @@
+package kingdom;
+
+public class Increment {
+	
+		int lvincrement(int lv) {
+			lv =lv+1;
+			return lv;
+		}
+		
+		int aincrement(int a) {
+			a =a+1;
+			return a;
+		}
+		
+	
+}	
