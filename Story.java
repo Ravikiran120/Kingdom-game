ae5b8709a83da23b389f76036f53d6a2f4b259eb
Ravikiran120 <85937120+Ravikiran120@users.noreply.github.com>
2021-06-22 20:32:26 +0530

    Add files via upload

---
+++ Story.java
@@ -0,0 +1,13 @@
+package kingdom;
+
+public class Story {
+	void createStory() {
+		System.out.println("HELLO KING....."
+				+ "\nYOU ARE THE KING OF THIS BURA BURA KINGDOM....."
+				+ "\nENEMY REGION'S KING HAS CONQUERED YOUR QUEEN"
+				+ "\nHE LOCKED HE IN ONE ROOM........"
+				+ "\nYOU SHOULD FIND HER ROOM ROOM AND RESCUE HER\n\n");
+		System.out.println("\n.....LEVEL 1.....\n\n");
+	}
+
+}
